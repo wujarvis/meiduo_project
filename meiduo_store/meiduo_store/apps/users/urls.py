001a205c25224ec9740106bb29c9c236e0ade9c0
wujarvis <mr_jarviswu@163.com>
2021-02-09 23:03:44 +0800

    用户登录后端逻辑实现

--- meiduo_store/meiduo_store/apps/users/urls.py
+++ meiduo_store/meiduo_store/apps/users/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^register/$', views.RegisterView.as_view(), name='register'),
     # 判断用户名是否重复注册
     url(r'^usernames/(?P<username>[a-zA-Z0-9_-]{5,20})/count/$', views.UsernameCountView.as_view()),
+    # 用户登录
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
 ]
--- meiduo_store/meiduo_store/apps/users/views.py
+++ meiduo_store/meiduo_store/apps/users/views.py
@@ -4,14 +4,67 @@ from django import http
 import re
 from django.db import DatabaseError
 from django.urls import reverse
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
+from django_redis import get_redis_connection
 
 from users.models import User
-from meiduo_store.utils.response_code import RETCODE
-
-
-
+from meiduo_mall.utils.response_code import RETCODE
 # Create your views here.
+
+
+class LoginView(View):
+    """用户登录"""
+
+    def get(self, request):
+        """提供用户登录页面"""
+        return render(request, 'login.html')
+
+    def post(self, request):
+        """实现用户登录逻辑"""
+        # 接收参数
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        remembered = request.POST.get('remembered')
+
+        # 校验参数
+        if not all([username, password]):
+            return http.HttpResponseForbidden('缺少必传参数')
+        if not re.match(r'^[a-zA-Z0-9_-]{5,20}$', username):
+            return http.HttpResponseForbidden('请输入正确的用户名或手机号')
+
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', password):
+            return http.HttpResponseForbidden('密码最少8位，最长20位')
+
+        # 认证用户:使用账号查询用户是否存在，如果用户存在，再校验密码是否正确
+        user = authenticate(username=username, password=password)
+        if user is None:
+            return render(request, 'login.html', {'account_errmsg': '账号或密码错误'})
+
+        # 状态保持
+        login(request, user)
+        # 使用remembered确定状态保持周期（实现记住登录）
+        if remembered != 'on':
+            # 没有记住登录：状态保持在浏览器会话结束后就销毁
+            request.session.set_expiry(0) # 单位是秒
+        else:
+            # 记住登录：状态保持周期为两周:默认是两周
+            request.session.set_expiry(None)
+
+        # 响应结果:重定向到首页
+        return redirect(reverse('contents:index'))
+
+
+class MobileCountView(View):
+    """判断手机号是否重复注册"""
+
+    def get(self, request, mobile):
+        """
+        :param mobile: 手机号
+        :return: JSON
+        """
+        count = User.objects.filter(mobile=mobile).count()
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'count': count})
+
 
 class UsernameCountView(View):
     """判断用户名是否重复注册"""
@@ -26,8 +79,10 @@ class UsernameCountView(View):
         # 响应结果
         return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'count': count})
 
+
 class RegisterView(View):
-    """用户注册页面"""
+    """用户注册"""
+
     def get(self, request):
         """提供用户注册页面"""
         return render(request, 'register.html')
@@ -39,6 +94,7 @@ class RegisterView(View):
         password = request.POST.get('password')
         password2 = request.POST.get('password2')
         mobile = request.POST.get('mobile')
+        sms_code_client = request.POST.get('sms_code')
         allow = request.POST.get('allow')
 
         # 校验参数：前后端的校验需要分开，避免恶意用户越过前端逻辑发请求，要保证后端的安全，前后端的校验逻辑相同
@@ -57,6 +113,13 @@ class RegisterView(View):
         # 判断手机号是否合法
         if not re.match(r'^1[3-9]\d{9}$', mobile):
             return http.HttpResponseForbidden('请输入正确的手机号码')
+        # 判断短信验证码是否输入正确
+        redis_conn = get_redis_connection('verify_code')
+        sms_code_server = redis_conn.get('sms_%s' % mobile)
+        if sms_code_server is None:
+            return render(request, 'register.html', {'sms_code_errmsg': '短信验证码已失效'})
+        if sms_code_client != sms_code_server.decode():
+            return render(request, 'register.html', {'sms_code_errmsg': '输入短信验证码有误'})
         # 判断是否勾选用户协议
         if allow != 'on':
             return http.HttpResponseForbidden('请勾选用户协议')
@@ -66,7 +129,7 @@ class RegisterView(View):
         try:
             user = User.objects.create_user(username=username, password=password, mobile=mobile)
         except DatabaseError:
-            return render(request, 'register.html', {'register_errmsg': '注册失败'})
+            return render(request, 'register.html', {'register_errmsg':'注册失败'})
 
         # 实现状态保持
         login(request, user)
