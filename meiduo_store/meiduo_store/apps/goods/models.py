c5336969c6587b6f9a812d5bee77d2317a0efff5
wujarvis <mr_jarviswu@163.com>
2021-02-14 19:26:12 +0800

    展示详情页数据

--- meiduo_store/meiduo_store/apps/goods/models.py
+++ meiduo_store/meiduo_store/apps/goods/models.py
@@ -4,7 +4,7 @@ from meiduo_store.utils.models import BaseModel
 # Create your models here.
 
 
-class GoodsCategory(BaseModel):
+class  GoodsCategory(BaseModel):
     """商品类别"""
     name = models.CharField(max_length=10, verbose_name='名称')
     parent = models.ForeignKey('self', related_name='subs', null=True, blank=True, on_delete=models.CASCADE, verbose_name='父类别')
